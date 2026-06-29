4d118c6a760d14210800e0939dfcc6914728018b
ayzayn <el.abdesslam@gmail.com>
2026-06-29 18:23:41 +0200

    BUG URL corrigé

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,10 +3,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from vehicles.views import TestAlertView # <-- L'importation requise est ajoutée ici
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('test-alert/', TestAlertView.as_view(), name='test-alert'), # <-- À mettre ici à la racine principale
+    path('test-alert/', TestAlertView.as_view(), name='test-alert'),
     path('api/', include('vehicles.urls')),
     path("api/", include("vehicles.urls")),
     path("api/", include("users.urls")),
